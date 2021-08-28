02b5f2d6e843e75dc5e266eda491a433abfbc788
John Bannister <johnb@hey.com>
2021-08-28 07:25:25 +0100

    Add routes command to output configured routes

--- cmd/main.go
+++ cmd/main.go
@@ -4,20 +4,73 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
+	"text/tabwriter"
 
 	"emailaddress.horse/thousand/app"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
 	app := app.NewApp()
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "4000"
+	cli := &cli.App{
+		Name:  "Thousand",
+		Usage: "I forget why I made this...",
+		Action: func(c *cli.Context) error {
+			port := os.Getenv("PORT")
+			if port == "" {
+				port = "4000"
+			}
+
+			addr := fmt.Sprintf(":%s", port)
+			if err := app.Start(addr); err != nil {
+				return err
+			}
+
+			return nil
+		},
+		Commands: []*cli.Command{
+			{
+				Name:  "routes",
+				Usage: "present a list of routes the app handles",
+				Action: func(c *cli.Context) error {
+					var methodOrder = map[string]int{
+						"GET":     0,
+						"POST":    1,
+						"PUT":     2,
+						"PATCH":   3,
+						"DELETE":  4,
+						"HEAD":    5,
+						"CONNECT": 6,
+						"OPTIONS": 7,
+						"TRACE":   8,
+					}
+
+					routes := app.Routes()
+
+					sort.Slice(routes, func(i, j int) bool {
+						if routes[i].Path == routes[j].Path {
+							return methodOrder[routes[i].Method] < methodOrder[routes[j].Method]
+						}
+
+						return routes[i].Path < routes[j].Path
+					})
+
+					writer := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', 0)
+					fmt.Fprintf(writer, "Method\tPath\tName\n")
+					for _, route := range routes {
+						fmt.Fprintf(writer, "%s\t%s\t%s\n", route.Method, route.Path, route.Name)
+					}
+					writer.Flush()
+
+					return nil
+				},
+			},
+		},
 	}
 
-	addr := fmt.Sprintf(":%s", port)
-	if err := app.Start(addr); err != nil {
+	if err := cli.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/labstack/echo/v4 v4.5.0
 	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/urfave/cli/v2 v2.3.0
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
 	golang.org/x/sys v0.0.0-20210823070655-63515b42dcdf // indirect
 	golang.org/x/text v0.3.7 // indirect
