503d6d70e6e6df5013fa19dd554169ad8df55289
John Bannister <johnb@hey.com>
2021-09-22 06:56:57 +0100

    Update echo to v4.6.0
    Simply test config setup and add names that express intention rather
    than type.

--- app/test_config.go
+++ app/test_config.go
@@ -1,23 +1,15 @@
 package app
 
 import (
-	"bytes"
 	"context"
 	"database/sql"
-	"encoding/json"
-	"fmt"
-	"io"
 	"os"
-	"strconv"
 	"strings"
-	"sync"
 	"testing"
-	"time"
 
 	"emailaddress.horse/thousand/app/models"
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/gommon/color"
-	"github.com/valyala/fasttemplate"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 // TestConfig sets up the app for running tests in a test environment by
@@ -26,18 +18,30 @@ import (
 func TestConfig(t *testing.T) EnvConfigurer {
 	return func(app *App) {
 		// Apply base config
-		BaseTestConfig(t)(app)
+		BaseTestConfigurer(t.Logf)(app)
 
 		// App configuration values
+		// Manually check for DATABASE_URL as tests cannot infer config from flags
+		// and environment variables through the CLI.
 		if os.Getenv("DATABASE_URL") != "" {
 			app.DatabaseURL = os.Getenv("DATABASE_URL")
 		}
-		app.DBConnector = txnDBConnector(t)
+		app.DBConnector = _transactionConnector(t)
 	}
 }
 
-// BaseTestConfig sets up the app for a test environment.
-func BaseTestConfig(t testLogger) EnvConfigurer {
+// LiveTestConfig sets up the app for a test environment with an adapter for the
+// usual (testing.T).Log and (testing.T).Logf which sends them to the app's
+// default Logger.
+var LiveTestConfig Configurer = EnvConfigurer(liveTestConfig)
+
+func liveTestConfig(app *App) {
+	// Apply base config
+	BaseTestConfigurer(app.Logger.Debugf)(app)
+}
+
+// BaseTestConfigurer sets up the app for a test environment.
+func BaseTestConfigurer(outf func(string, ...interface{})) EnvConfigurer {
 	return func(app *App) {
 		// Echo configuraton values
 		app.Debug = true
@@ -46,12 +50,12 @@ func BaseTestConfig(t testLogger) EnvConfigurer {
 		app.DatabaseURL = "postgres://localhost:5432/thousand_test?sslmode=disable"
 
 		// Injected middleware
-		app.LoggerMiddleware = _loggerWithConfig(_testLogWriter{t})
-		app.HTTPErrorHandler = _httpErrorHandler(t, app.DefaultHTTPErrorHandler)
+		app.LoggerMiddleware = _readableLogger(outf)
+		app.HTTPErrorHandler = _logError(outf, app.HTTPErrorHandler)
 	}
 }
 
-func txnDBConnector(t *testing.T) DBConnector {
+func _transactionConnector(t *testing.T) DBConnector {
 	return func(databaseURL string) (models.DBTX, error) {
 		conn, err := sql.Open("postgres", databaseURL)
 		if err != nil {
@@ -73,176 +77,26 @@ func txnDBConnector(t *testing.T) DBConnector {
 	}
 }
 
-type testLogger interface {
-	Log(...interface{})
-	Logf(string, ...interface{})
-}
-
-type _testLogWriter struct{ testLogger }
-
-func (tl _testLogWriter) Write(p []byte) (int, error) {
-	tl.Logf(string(p))
-	return len(p), nil
-}
-
-// Extracted from github.com/labstack/echo/v4/middleware LoggerWithConfig to
-// allow padding the method for easier to read output.
-func _loggerWithConfig(output io.Writer) echo.MiddlewareFunc {
-	skipper := func(c echo.Context) bool {
-		return strings.HasPrefix(c.Path(), "/css")
-	}
-	format := "${status} ${method} ${uri}\n"
-	customTimeFormat := "2006-01-02 15:04:05.00000"
-	template := fasttemplate.New(format, "${", "}")
-	colorer := color.New()
-	colorer.SetOutput(output)
-	pool := &sync.Pool{
-		New: func() interface{} {
-			return bytes.NewBuffer(make([]byte, 256))
+func _readableLogger(outf func(string, ...interface{})) echo.MiddlewareFunc {
+	return middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		Skipper: func(c echo.Context) bool {
+			return strings.HasPrefix(c.Path(), "/assets/")
 		},
-	}
 
-	return func(next echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) (err error) {
-			if skipper(c) {
-				return next(c)
-			}
-
-			req := c.Request()
-			res := c.Response()
-			start := time.Now()
-			if err = next(c); err != nil {
-				c.Error(err)
-			}
-			stop := time.Now()
-			buf := pool.Get().(*bytes.Buffer)
-			buf.Reset()
-			defer pool.Put(buf)
-
-			if _, err = template.ExecuteFunc(buf, func(w io.Writer, tag string) (int, error) {
-				switch tag {
-				case "time_unix":
-					return buf.WriteString(strconv.FormatInt(time.Now().Unix(), 10))
-				case "time_unix_nano":
-					return buf.WriteString(strconv.FormatInt(time.Now().UnixNano(), 10))
-				case "time_rfc3339":
-					return buf.WriteString(time.Now().Format(time.RFC3339))
-				case "time_rfc3339_nano":
-					return buf.WriteString(time.Now().Format(time.RFC3339Nano))
-				case "time_custom":
-					return buf.WriteString(time.Now().Format(customTimeFormat))
-				case "id":
-					id := req.Header.Get(echo.HeaderXRequestID)
-					if id == "" {
-						id = res.Header().Get(echo.HeaderXRequestID)
-					}
-					return buf.WriteString(id)
-				case "remote_ip":
-					return buf.WriteString(c.RealIP())
-				case "host":
-					return buf.WriteString(req.Host)
-				case "uri":
-					return buf.WriteString(req.RequestURI)
-				case "method":
-					return buf.WriteString(fmt.Sprintf("%-8s", req.Method))
-				case "path":
-					p := req.URL.Path
-					if p == "" {
-						p = "/"
-					}
-					return buf.WriteString(p)
-				case "protocol":
-					return buf.WriteString(req.Proto)
-				case "referer":
-					return buf.WriteString(req.Referer())
-				case "user_agent":
-					return buf.WriteString(req.UserAgent())
-				case "status":
-					n := res.Status
-					s := colorer.Green(n)
-					switch {
-					case n >= 500:
-						s = colorer.Red(n)
-					case n >= 400:
-						s = colorer.Yellow(n)
-					case n >= 300:
-						s = colorer.Cyan(n)
-					}
-					return buf.WriteString(s)
-				case "error":
-					if err != nil {
-						// Error may contain invalid JSON e.g. `"`
-						b, _ := json.Marshal(err.Error())
-						b = b[1 : len(b)-1]
-						return buf.Write(b)
-					}
-				case "latency":
-					l := stop.Sub(start)
-					return buf.WriteString(strconv.FormatInt(int64(l), 10))
-				case "latency_human":
-					return buf.WriteString(stop.Sub(start).String())
-				case "bytes_in":
-					cl := req.Header.Get(echo.HeaderContentLength)
-					if cl == "" {
-						cl = "0"
-					}
-					return buf.WriteString(cl)
-				case "bytes_out":
-					return buf.WriteString(strconv.FormatInt(res.Size, 10))
-				default:
-					switch {
-					case strings.HasPrefix(tag, "header:"):
-						return buf.Write([]byte(c.Request().Header.Get(tag[7:])))
-					case strings.HasPrefix(tag, "query:"):
-						return buf.Write([]byte(c.QueryParam(tag[6:])))
-					case strings.HasPrefix(tag, "form:"):
-						return buf.Write([]byte(c.FormValue(tag[5:])))
-					case strings.HasPrefix(tag, "cookie:"):
-						cookie, err := c.Cookie(tag[7:])
-						if err == nil {
-							return buf.Write([]byte(cookie.Value))
-						}
-					}
-				}
-				return 0, nil
-			}); err != nil {
-				return
-			}
-
-			_, err = output.Write(buf.Bytes())
-			return
-		}
-	}
+		LogValuesFunc: func(c echo.Context, values middleware.RequestLoggerValues) error {
+			outf("%d %-8s %s", values.Status, values.Method, values.URI)
+			return nil
+		},
+		LogMethod: true,
+		LogURI:    true,
+		LogStatus: true,
+	})
 }
 
-func _httpErrorHandler(t testLogger, handler echo.HTTPErrorHandler) echo.HTTPErrorHandler {
+func _logError(outf func(string, ...interface{}), handler echo.HTTPErrorHandler) echo.HTTPErrorHandler {
 	return func(err error, c echo.Context) {
-		t.Log(err)
+		outf("%w", err)
 
 		handler(err, c)
 	}
-}
-
-// LiveTestConfig sets up the app for a test environment with an adapter for the
-// usual (testing.T).Log and (testing.T).Logf which sends them to the app's
-// default Logger.
-var LiveTestConfig Configurer = EnvConfigurer(liveTestConfig)
-
-func liveTestConfig(app *App) {
-	// Apply base config
-	BaseTestConfig(&_liveTestLogger{app.Logger})(app)
-}
-
-var _ testLogger = (*_liveTestLogger)(nil)
-
-type _liveTestLogger struct {
-	echo.Logger
-}
-
-func (logger *_liveTestLogger) Log(args ...interface{}) {
-	logger.Debug(args...)
-}
-
-func (logger *_liveTestLogger) Logf(format string, args ...interface{}) {
-	logger.Debugf(format, args...)
 }
--- go.mod
+++ go.mod
@@ -8,16 +8,17 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/google/go-cmp v0.5.6
 	github.com/google/uuid v1.3.0
-	github.com/labstack/echo/v4 v4.5.0
+	github.com/labstack/echo/v4 v4.6.0
 	github.com/labstack/gommon v0.3.0
 	github.com/lib/pq v1.10.3
-	github.com/mattn/go-isatty v0.0.13 // indirect
+	github.com/mattn/go-colorable v0.1.9 // indirect
 	github.com/pressly/goose/v3 v3.1.0
 	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/urfave/cli/v2 v2.3.0
 	github.com/valyala/fasttemplate v1.2.1
-	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
-	golang.org/x/sys v0.0.0-20210823070655-63515b42dcdf // indirect
-	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/net v0.0.0-20210917221730-978cfadd31cf // indirect
+	golang.org/x/sys v0.0.0-20210921065528-437939a70204 // indirect
+	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
